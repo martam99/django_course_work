bc5217e1561ab206d79ffe0e6017c959b64224dc
Mariam Tamrazyan <mart76901@gmail.com>
2023-09-28 17:48:06 +0400

    Добавлены переменные окружения для создания суперпользователя

--- user/management/commands/csu.py
+++ user/management/commands/csu.py
@@ -1,18 +1,23 @@
+import os
+from dotenv import load_dotenv
 from django.core.management import BaseCommand
 
+from config.settings import BASE_DIR
 from user.models import User
+
+load_dotenv(BASE_DIR / '.env')
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='maradmin@mail.ru',
-            first_name='Mariam',
-            last_name='Tamrazyan',
+            email=os.getenv("email"),
+            first_name=os.getenv("first_name"),
+            last_name=os.getenv("last_name"),
             is_staff=True,
             is_superuser=True,
             is_active=True
         )
 
-        user.set_password('mmm111+++')
+        user.set_password(os.getenv('password'))
         user.save()
